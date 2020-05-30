7cf680e4b32510872cddacf16089344f67d9f2c6
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-05-29 21:35:11 -0400

    Add new line for open braces

--- cssbeauti.py
+++ cssbeauti.py
@@ -2,9 +2,8 @@ import re
 
 with open("messy.css", "r") as f:
     for line in f:
-        for m in re.finditer("{", line):
-            #print("%02d-%02d: %s" % (m.start(), m.end(), m.group()))
-            print(re.sub("\s*{", " { ", line))
-            #print(line.replace(line[m.start():m.end()], m.group() + "\n"))
-            #line = line.replace(line[m.start():m.end()], m.group() + "\n")
+        line = re.sub("\s*{\s*", " {", line)
+        for m in re.finditer("\w+\s*{\s*", line):
+            line = line.replace(line[m.start():m.end()], m.group() + "\n")
+        print(line)
 f.close()

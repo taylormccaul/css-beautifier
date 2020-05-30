ba019c599a0f5f4cf54fd6ce5bd9ff99cc3bc532
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-05-29 21:42:56 -0400

    Add new lines after closing brace

--- cssbeauti.py
+++ cssbeauti.py
@@ -2,8 +2,6 @@ import re
 
 with open("messy.css", "r") as f:
     for line in f:
-        line = re.sub("\s*{\s*", " {", line)
-        for m in re.finditer("\w+\s*{\s*", line):
-            line = line.replace(line[m.start():m.end()], m.group() + "\n")
-        print(line)
+        line = re.sub("\s*{\s*", " {\n", line)
+        line = re.sub("\s*}\s*", "\n}\n\n", line)
 f.close()

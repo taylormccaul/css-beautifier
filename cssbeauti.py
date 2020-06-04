1654721e8593e98666088df7d54e1b8fba916d8d
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-06-03 21:26:18 -0400

    Add new lines after semicolons

--- cssbeauti.py
+++ cssbeauti.py
@@ -4,4 +4,14 @@ with open("messy.css", "r") as f:
     for line in f:
         line = re.sub("\s*{\s*", " {\n", line)
         line = re.sub("\s*}\s*", "\n}\n\n", line)
+        line = re.sub(";\s*\s*", ";\n", line)
+        """for m in re.finditer(";\s*[A-Za-z]+\s*[\n]{0}", line):
+            y = m.start()
+            z = m.end()
+            print(line[y])
+            print(line.replace(line[y], ";\nS"))
+            #line = line.replace(line[y:y+1], ";\n")
+            #print(line.replace(line[y:y+1], ";\n"))
+            #print(line.strip())"""
+        print(line)
 f.close()

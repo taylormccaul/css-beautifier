fcfc309ee10b32395f669ff65ae339e4018c1b16
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-06-03 22:06:34 -0400

    Update cssbeauti.py

--- cssbeauti.py
+++ cssbeauti.py
@@ -1,17 +1,10 @@
 import re
 
-with open("messy.css", "r") as f:
+with open("messy.css", "r+") as f:
     for line in f:
         line = re.sub("\s*{\s*", " {\n", line)
         line = re.sub("\s*}\s*", "\n}\n\n", line)
         line = re.sub(";\s*\s*", ";\n", line)
-        """for m in re.finditer(";\s*[A-Za-z]+\s*[\n]{0}", line):
-            y = m.start()
-            z = m.end()
-            print(line[y])
-            print(line.replace(line[y], ";\nS"))
-            #line = line.replace(line[y:y+1], ";\n")
-            #print(line.replace(line[y:y+1], ";\n"))
-            #print(line.strip())"""
-        print(line)
+        line = re.sub(":\s{0,}",  ": ", line)
+        f.write(line)
 f.close()
